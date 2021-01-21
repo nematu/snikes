bec7926864c6d1d697bee5bbe86218073e4e5c4c
nematu <nayaz-akhundzada@mail.greenriver.edu>
2021-01-21 15:03:14 -0500

    added view file

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+// TThis is my CONTROLLER
+
+//Turn on Error reporting
+ini_set('display_errors',TRUE);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+// Create an instance of the Base class
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+
+//Define a default route (home page of our application)
+$f3->route('GET /', function (){
+  $view = new Template();
+  echo $view->render('views/home.html');
+});
+
+// Run fat free
+$f3->run();
